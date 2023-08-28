8d802e2e4fba65bb2377f90adce0e002460b849d
WooSungD <woosung@float.capital>
2023-08-28 15:23:10 +0100

    WIP: tried implementing style from refine

--- src/theme/BlogListPage/index.js
+++ src/theme/BlogListPage/index.js
@@ -9,10 +9,13 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import BlogPostItem from '@theme/BlogPostItem';
 import BlogListPaginator from '@theme/BlogListPaginator';
-import {ThemeClassNames} from '@docusaurus/theme-common';
+import {ThemeClassNames, HtmlClassNameProvider} from '@docusaurus/theme-common';
+import { BlogPostProvider } from "@docusaurus/theme-common/internal";
 
 function BlogListPage(props) {
   const {metadata, items, sidebar} = props;
+  // console.log("BlogListPage metadata: ", metadata);
+
 
   console.log("BlogListPage props: ", props);
   const {
@@ -22,6 +25,7 @@ function BlogListPage(props) {
   const isBlogOnlyMode = permalink === '/';
   const title = isBlogOnlyMode ? siteTitle : blogTitle;
   return (
+    
     <Layout
       title={title}
       description={blogDescription}
@@ -35,13 +39,17 @@ function BlogListPage(props) {
         <div className="row">
           {items.map(({content: BlogPostContent}) => (
             <aside className="col col--4">
-              <BlogPostItem
-                key={BlogPostContent.metadata.permalink}
-                frontMatter={BlogPostContent.frontMatter}
-                metadata={BlogPostContent.metadata}
-                truncated={BlogPostContent.metadata.truncated}>
-                <BlogPostContent />
-              </BlogPostItem>
+                <HtmlClassNameProvider> 
+                  <BlogPostProvider>
+                    <BlogPostItem
+                      key={BlogPostContent.metadata.permalink}
+                      frontMatter={BlogPostContent.frontMatter}
+                      metadata={BlogPostContent.metadata}
+                      truncated={BlogPostContent.metadata.truncated}>
+                      <BlogPostContent />
+                    </BlogPostItem>
+                  </BlogPostProvider>
+                </HtmlClassNameProvider>
             </aside> 
           ))}
         </div>
