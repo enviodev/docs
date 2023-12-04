6b88dd174d863199cf70f46754a78f506db08725
SvenMueller95 <sven@float.capital>
2023-12-04 17:24:12 +0200

    add token vault indexer to examples

--- sidebars.js
+++ sidebars.js
@@ -18,6 +18,7 @@ module.exports = {
             "example-uniswap-v3",
             "example-aave-token",
             "example-reNFT",
+            "example-token-vaults"
           ],
         },
         {
