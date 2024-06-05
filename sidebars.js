b74d87b89c5020941e6cdd67d14297abe59bccba
Dmitry Zakharov <dzakh.dev@gmail.com>
2024-06-05 14:46:30 +0400

    Add tutorial to the sidebar

--- sidebars.js
+++ sidebars.js
@@ -14,6 +14,7 @@ module.exports = {
           items: [
             "tutorial-op-bridge-deposits",
             "tutorial-erc20-token-transfers",
+            "tutorial-indexing-fuel",
             "greeter-tutorial",
           ],
         },
