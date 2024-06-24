ab352058379c984fd666e0903ddea780036171ad
Jason <jason@envio.dev>
2024-06-24 12:15:13 +0200

    Add redirects and cleanup repo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,6 +2,201 @@ const { themes } = require('prism-react-renderer');
 const lightCodeTheme = themes.github;
 const darkCodeTheme = themes.dracula;
 
+const redirectsList = [
+  {
+    from: "/docs/quickstart",
+    to: "/docs/HyperIndex/overview",
+  },
+  {
+    from: "/docs/overview",
+    to: "/docs/HyperIndex/overview",
+  },
+  {
+    from: "/docs/getting-started",
+    to: "/docs/HyperIndex/getting-started",
+  },
+  {
+    from: "/docs/contract-import",
+    to: "/docs/HyperIndex/contract-import",
+  },
+  {
+    from: "/docs/hyperindex-basics",
+    to: "/docs/HyperIndex/hyperindex-basics",
+  },
+  {
+    from: "/docs/configuration-file",
+    to: "/docs/HyperIndex/configuration-file",
+  },
+  {
+    from: "/docs/schema",
+    to: "/docs/HyperIndex/schema",
+  },
+  {
+    from: "/docs/event-handlers",
+    to: "/docs/HyperIndex/event-handlers",
+  },
+  {
+    from: "/docs/testing",
+    to: "/docs/HyperIndex/testing",
+  },
+  {
+    from: "/docs/running-locally",
+    to: "/docs/HyperIndex/running-locally",
+  },
+  {
+    from: "/docs/navigating-hasura",
+    to: "/docs/HyperIndex/navigating-hasura",
+  },
+  {
+    from: "/docs/cli-commands",
+    to: "/docs/HyperIndex/cli-commands",
+  },
+  {
+    from: "/docs/hosted-service",
+    to: "/docs/HyperIndex/hosted-service",
+  },
+  {
+    from: "/docs/hosted-service-deployment",
+    to: "/docs/HyperIndex/hosted-service-deployment",
+  },
+  {
+    from: "/docs/hosted-service-billing",
+    to: "/docs/HyperIndex/hosted-service-billing",
+  },
+  {
+    from: "/docs/licensing",
+    to: "/docs/HyperIndex/licensing",
+  },
+  {
+    from: "/docs/fuel",
+    to: "/docs/HyperIndex/fuel",
+  },
+  {
+    from: "/docs/tutorial-op-bridge-deposits",
+    to: "/docs/HyperIndex/tutorial-op-bridge-deposits",
+  },
+  {
+    from: "/docs/tutorial-erc20-token-transfers",
+    to: "/docs/HyperIndex/tutorial-erc20-token-transfers",
+  },
+  {
+    from: "/docs/tutorial-indexing-fuel",
+    to: "/docs/HyperIndex/tutorial-indexing-fuel",
+  },
+  {
+    from: "/docs/greeter-tutorial",
+    to: "/docs/HyperIndex/greeter-tutorial",
+  },
+  {
+    from: "/docs/linked-entity-loaders",
+    to: "/docs/HyperIndex/linked-entity-loaders",
+  },
+  {
+    from: "/docs/dynamic-contracts",
+    to: "/docs/HyperIndex/dynamic-contracts",
+  },
+  {
+    from: "/docs/multichain-indexing",
+    to: "/docs/HyperIndex/multichain-indexing",
+  },
+  {
+    from: "/docs/hypersync/",
+    to: "/docs/HyperIndex/hypersync",
+  },
+  {
+    from: "/docs/rpc-sync",
+    to: "/docs/HyperIndex/rpc-sync",
+  },
+  {
+    from: "/docs/persisted_files",
+    to: "/docs/HyperIndex/persisted_files",
+  },
+  {
+    from: "/docs/generated-files",
+    to: "/docs/HyperIndex/generated-files",
+  },
+  {
+    from: "/docs/terminology",
+    to: "/docs/HyperIndex/terminology",
+  },
+  {
+    from: "/docs/async-mode",
+    to: "/docs/HyperIndex/async-mode",
+  },
+  {
+    from: "/docs/labels",
+    to: "/docs/HyperIndex/labels",
+  },
+  {
+    from: "/docs/performance",
+    to: "/docs/HyperIndex/performance",
+  },
+  {
+    from: "/docs/example-sablier-v2",
+    to: "/docs/HyperIndex/example-sablier-v2",
+  },
+  {
+    from: "/docs/example-liquidation-metrics",
+    to: "/docs/HyperIndex/example-liquidation-metrics",
+  },
+  {
+    from: "/docs/example-uniswap-v3",
+    to: "/docs/HyperIndex/example-uniswap-v3",
+  },
+  {
+    from: "/docs/example-ens",
+    to: "/docs/HyperIndex/example-ens",
+  },
+  {
+    from: "/docs/logging",
+    to: "/docs/HyperIndex/logging",
+  },
+  {
+    from: "/docs/common-issues",
+    to: "/docs/HyperIndex/common-issues",
+  },
+  {
+    from: "/docs/error-codes",
+    to: "/docs/HyperIndex/error-codes",
+  },
+  {
+    from: "/docs/reserved-words",
+    to: "/docs/HyperIndex/reserved-words",
+  },
+  //// HyperSync
+  {
+    from: "/docs/overview-hypersync",
+    to: "/docs/HyperSync/overview",
+  },
+  {
+    from: "/docs/hypersync-usage",
+    to: "/docs/HyperSync/hypersync-usage",
+  },
+  {
+    from: "/docs/hypersync-query",
+    to: "/docs/HyperSync/hypersync-query",
+  },
+  {
+    from: "/docs/hypersync-clients",
+    to: "/docs/HyperSync/hypersync-clients",
+  },
+  {
+    from: "/docs/hypersync-curl-example",
+    to: "/docs/HyperSync/hypersync-curl-examples",
+  },
+  {
+    from: "/docs/hypersync-url-endpoints",
+    to: "/docs/HyperSync/hypersync-url-endpoints",
+  },
+  {
+    from: "/docs/hyperfuel-query",
+    to: "/docs/HyperSync/hyperfuel-query",
+  },
+  {
+    from: "/docs/overview-hyperrpc",
+    to: "/docs/HyperSync/overview-hyperrpc",
+  },
+]
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Envio",
@@ -161,6 +356,12 @@ const config = {
         //     path: '1.2.0',
         //   },
         // },
+      },
+    ],
+    [
+      "@docusaurus/plugin-client-redirects",
+      {
+        redirects: redirectsList,
       },
     ],
   ],
